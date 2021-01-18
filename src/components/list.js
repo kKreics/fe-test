4af09e8830f0a5837207cbfc5742fe72be60ea4b
kKreics <kristskreics@gmail.com>
2021-01-18 19:51:44 +0200

    Initial commit

---
+++ src/components/list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const List = ({ items }) => {
+  console.log(items);
+
+  return (
+    <h1>List items should appear below</h1>
+  );
+};
+
+export default List;
---
+++ src/data/dummy-items.js
@@ -0,0 +1,30 @@
+const DUMMY_ITEMS = [{
+  id: '123',
+  name: 'dummy 1',
+}, {
+  id: '1223',
+  name: 'dummy 2',
+}, {
+  id: '1223',
+  name: 'dummy 3',
+}, {
+  id: '112323',
+  name: 'dummy 4',
+}, {
+  id: '15523',
+  name: 'dummy 5',
+}, {
+  id: '121123',
+  name: 'dummy 6',
+}, {
+  id: '120003',
+  name: 'dummy 7',
+}, {
+  id: '1023',
+  name: 'dummy 8',
+}, {
+  id: '129993',
+  name: 'dummy 9',
+}];
+
+export default DUMMY_ITEMS;
